79b953270bf0cafcd4b21fd46e42bcebba41452e
snx23d <antotalgin@gmail.com>
2020-02-01 20:42:32 +0100

    prep for task2

--- opi/src/main/java/tasks/guice/ProjectModule.java
+++ opi/src/main/java/tasks/guice/ProjectModule.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebDriver;
 public final class ProjectModule extends AbstractModule {
     @Override
     public void configure() {
+        //I really don't like the fact I have to use Singleton here
         bind(WebDriver.class).toProvider(WebDriverProvider.class).in(Singleton.class);
     }
 }
--- opi/src/test/java/tasks/acceptancetests/Task2Unit.java
+++ opi/src/test/java/tasks/acceptancetests/Task2Unit.java
@@ -5,6 +5,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+
+
+
 
 import static utils.utils.*;
 
